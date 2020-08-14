5169a2305540a4a2e8e9a98966a4f9138f5e3cee
JakhongirGaniev <69113613+JakhongirGaniev@users.noreply.github.com>
2020-08-14 08:51:32 +0500

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -20,7 +20,7 @@ def _start(c, m):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Join Project Channel!', url='https://t.me/odbots')
+                    InlineKeyboardButton('🚥Subscribe To My Channel!🙏', url='https://youtube.com/channel/UCOUMWBgL80w6SwG2T6wJbIQ')
                 ]
             ]
         )
